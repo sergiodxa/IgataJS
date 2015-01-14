737efb6b1b89fef2b59e6eac2cfb11b3913f39e5
Sergio Xalambrí <sergio@xalambri.com.ar>
2015-01-13 21:41:05 -0300

    readme and library

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # IgataJS
-Template engine who work with the HTML5 template tag
+Template engine who work with the HTML5 template tag.
+
+Test coming soon!
+
+## Install
+Use NPM to install with the command:
+
+```
+npm i -S igatajs
+```
+
+## How to use
+Import IgataJS inside your JS file using:
+
+```javascript
+var igatajs = require('igatajs');
+```
+
+In your HTML files create your templates:
+
+```html
+<template id="myTemplate">
+  <h1>{{title}}</h1>
+</template>
+```
+
+In your JS file now use IgataJS with these lines:
+
+```
+var template = igatajs('#myTemplate', {
+  title: 'Hello world'
+});
+document.body.appendChild(template);
+```
+
+Now compile your JS using Browserify to create a browser compatible JS file.
+
+Enjoy!
+
+## License
+The MIT License (MIT)
+
+Copyright (c) 2015 Sergio Daniel Xalambrí
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ index.js
@@ -0,0 +1,78 @@
+var igatajs = function (templateID, dataSet, opening, closing) {
+  try {
+    // check if the templateID is a valid string
+    if (typeof templateID !== 'string') {
+      return new Error('The template id must be a string.');
+    }
+    // check if dataSet is defined and is a valid object
+    if (dataSet && typeof dataSet !== 'object') {
+      return new Error('The dataset must be an object.');
+    }
+    // check if the opening symbol is defined and is a string
+    if (opening && typeof opening !== 'string') {
+      return new Error('The opening symbol must be a string.');
+    }
+    // check if the closing symbol is defined and is a string
+    if (closing && typeof closing !== 'string') {
+      return new Error('The closing symbol must be a string.');
+    }
+
+    // if the opening or closing symbol isn't defined there are equal to {{ and }}
+    var opening = opening || '{{';
+    var closing = closing || '}}';
+
+    // function to put the dataSet inside the template
+    function parseString(string, dataSet, opening, closing) {
+      try {
+        var open = opening.replace(/[-[\]()*\s]/g, "\\$&").replace(/\$/g, '\\$')
+        var close = closing.replace(/[-[\]()*\s]/g, "\\$&").replace(/\$/g, '\\$')
+        var r = new RegExp(open + '(.+?)' + close, 'g')
+        var matches = string.match(r) || [];
+
+        matches.forEach(function(match) {
+          var key = match.substring(opening.length, match.length - closing.length).trim();//chop {{ and }}
+          var value = typeof dataSet[key] == 'undefined' ? '' : dataSet[key];
+          string = string.replace(match, value);
+        });
+
+        return string;
+      } catch (err) {
+        return err;
+      }
+    }
+
+    // add # if the templateID does not have it
+    if (templateID.indexOf('#') === -1) {
+      templateID = '#' + templateID;
+    }
+
+    // get the template
+    var template = document.querySelector(templateID);
+    var clone;
+
+    // if the user pass a dataSet, replace it in the clone
+    if (dataSet) {
+      // get the innerHTML as string
+      clone    = template.innerHTML.trim()
+      clone = parseString(clone, dataSet, opening, closing);
+      // create de dom parser
+      var parser = new DOMParser();
+
+      // parse clone to DOM Element
+      clone = parser.parseFromString(clone, 'text/html').all[2]
+
+      // get the correct element
+      clone = clone.children[0]
+    } else {
+      clone = document.importNode(template.content, true)
+    }
+
+    // return the clone
+    return clone;
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+}
+
+module.exports = igatajs;
